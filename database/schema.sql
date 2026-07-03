02ca29115f1761b3a1efeff18fd050865fa023a8
wissenfuerkleine-hash <wissenfuerkleine@gmail.com>
2026-07-03 12:53:28 +0200

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,3 @@
-CREATE TABLE IF NOT EXISTS active_lockdown (
-  id INT PRIMARY KEY DEFAULT 1,
-  incident_id TEXT,
-  level INT,
-  reason TEXT
-);
-
 CREATE TABLE IF NOT EXISTS snapshots (
   incident_id TEXT PRIMARY KEY,
   data TEXT NOT NULL
